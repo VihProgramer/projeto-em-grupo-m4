2d52eba2a5d936fed5aa60bb9b51846cce149c97
Diego Bernardes <113109526+DiegoBernardes95@users.noreply.github.com>
2023-02-04 16:29:39 -0300

    Print de modelagem adicionado, tabelas atualizadas.

--- SQL/temporadas.sql
+++ SQL/temporadas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 03-Fev-2023 às 04:01
+-- Tempo de geração: 04-Fev-2023 às 19:49
 -- Versão do servidor: 10.4.27-MariaDB
 -- versão do PHP: 8.2.0
 
